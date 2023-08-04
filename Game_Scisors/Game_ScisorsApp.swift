c16ff41c672afae968874d1d94f2b1885597bb19
newN <newN@gmil.com>
2023-08-04 15:46:58 -0600

    Initial Commit

---
+++ Game_Scisors/Game_ScisorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_ScisorsApp.swift
+//  Game_Scisors
+//
+//  Created by Alumno on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Game_ScisorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
